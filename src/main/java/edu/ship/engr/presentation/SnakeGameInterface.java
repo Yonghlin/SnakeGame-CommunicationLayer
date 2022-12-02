3f71b3f57653ca0115f401ec7095110c8e50cadd
Ktyal Plummer <ktyalplummer@gmail.com>
2022-12-02 15:00:51 -0500

    Small fixes

--- src/main/java/edu/ship/engr/presentation/SnakeGameInterface.java
+++ src/main/java/edu/ship/engr/presentation/SnakeGameInterface.java
@@ -3,6 +3,6 @@ package edu.ship.engr.presentation;
 import edu.ship.engr.presentation.gameobjects.Rectangle;
 
 public interface SnakeGameInterface {
-    public void startGame();
-    public void checkAppleCollision(Rectangle snakeHead);
+    void startGame();
+    void checkAppleCollision(Rectangle snakeHead);
 }
